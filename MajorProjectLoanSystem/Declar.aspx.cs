2251129a1dd0f98eb5b7ebbb1c9d5945454aa57d
harshbaisware <harshb5604@gmail.com>
2021-01-13 17:56:40 +0530

    commit 2

---
+++ MajorProjectLoanSystem/Declar.aspx.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace MajorProjectLoanSystem
+{
+    public partial class Declar : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
